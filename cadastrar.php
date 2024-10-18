7caf27339495dce3309fa0c0ac40238fa71e4eeb
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-10-17 22:58:29 -0300

    atualiazações

--- cadastrar.php
+++ cadastrar.php
@@ -11,22 +11,22 @@
 			<form action="php_action/criar_clientes.php" method="POST">
 				
 				<div class="input-field col s12">
-					<input type="text" name="nome" id="nome">
+					<input type="text" name="nome" id="nome" required >
 					<label for="nome">Nome</label>
 				</div>
 				
 				<div class="input-field col s12">
-					<input type="text" name="sobrenome" id="sobrenome">
+					<input type="text" name="sobrenome" id="sobrenome" required >
 					<label for="sobrenome">Sobrenome</label>
 				</div>
 				
 				<div class="input-field col s12">
-					<input type="text" name="email" id="email">
+					<input type="text" name="email" id="email" required >
 					<label for="email">E-mail</label>
 				</div>
 				
 				<div class="input-field col s12">
-					<input type="text" name="idade" id="idade">
+					<input type="text" name="idade" id="idade" required >
 					<label for="idade">Idade</label>
 				</div>
 
